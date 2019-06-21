f8edf732cf18ad62570233e2c77cbb42c61309fc
Marko Arsic <marsicdev@gmail.com>
2019-06-21 11:49:55 +0200

    Update project and dependencies

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom'
 import { HashRouter } from 'react-router-dom'
 
 import './index.css'
-import 'materialize-css/dist/css/materialize.css'
 
 import App from './app/App'
 
